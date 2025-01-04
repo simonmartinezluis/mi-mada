4af9cfa8f00e49236fbb63310581816b47b7fbdc
Luis Simon Martinez <simonmartinez.luis@gmail.com>
2025-01-04 16:57:31 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// primeNG
+import { ButtonModule } from 'primeng/button';
+
+import {StyleClassModule} from 'primeng/styleclass';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule,
+    StyleClassModule
   ],
   providers: [],
   bootstrap: [AppComponent]
